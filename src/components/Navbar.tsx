613e7d394fec7163fce8a2aa3ed7f09059841c38
engmohamedtarek1 <eng.mohamedtarek0@gmail.com>
2024-10-19 01:12:03 +0300

    Edit close menu to be sticky at the navigations menu top

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import logo from "../assets/shared/logo.svg";
 const Navbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
   const location = useLocation();
-  
+
   return (
     <header className="flex items-center justify-between py-6 md:pt-0 lg:pt-10">
       <div className="flex items-center px-10 lg:pl-16 lg:pr-0">
@@ -20,7 +20,7 @@ const Navbar = () => {
         {isMobileMenuOpen ? (
           <IoMdClose
             size={30}
-            className="cursor-pointer text-white"
+            className="cursor-pointer text-white fixed top-8 right-10 z-50"
             onClick={() => setIsMobileMenuOpen(false)}
           />
         ) : (
@@ -36,7 +36,9 @@ const Navbar = () => {
         <nav className="fixed right-0 top-0 z-40 h-full w-3/4 p-8 pt-20 backdrop-blur-lg">
           <ul className="flex flex-col gap-6 text-300">
             <li
-              className={`underline-indicators ${location.pathname === "/" ? "active" : ""}`}
+              className={`underline-indicators ${
+                location.pathname === "/" ? "active" : ""
+              }`}
             >
               <Link
                 to="/"
@@ -47,7 +49,9 @@ const Navbar = () => {
               </Link>
             </li>
             <li
-              className={`underline-indicators ${location.pathname.startsWith("/destination") ? "active" : ""}`}
+              className={`underline-indicators ${
+                location.pathname.startsWith("/destination") ? "active" : ""
+              }`}
             >
               <Link
                 to="/destination"
@@ -58,7 +62,9 @@ const Navbar = () => {
               </Link>
             </li>
             <li
-              className={`underline-indicators ${location.pathname.startsWith("/crew") ? "active" : ""}`}
+              className={`underline-indicators ${
+                location.pathname.startsWith("/crew") ? "active" : ""
+              }`}
             >
               <Link
                 to="/crew"
@@ -69,7 +75,9 @@ const Navbar = () => {
               </Link>
             </li>
             <li
-              className={`underline-indicators ${location.pathname.startsWith("/technology") ? "active" : ""}`}
+              className={`underline-indicators ${
+                location.pathname.startsWith("/technology") ? "active" : ""
+              }`}
             >
               <Link
                 to="/technology"
@@ -87,30 +95,41 @@ const Navbar = () => {
       <nav className="hidden bg-white/5 px-10 py-8 md:block lg:px-16 lg:backdrop-blur-lg">
         <ul className="flex gap-6 text-300">
           <li
-            className={`underline-indicators ${location.pathname === "/" ? "active" : ""}`}
+            className={`underline-indicators ${
+              location.pathname === "/" ? "active" : ""
+            }`}
           >
             <Link to="/" className="uppercase tracking-2 text-white">
               <span className="mr-3 font-bold">00</span>Home
             </Link>
           </li>
           <li
-            className={`underline-indicators ${location.pathname.startsWith("/destination") ? "active" : ""}`}
+            className={`underline-indicators ${
+              location.pathname.startsWith("/destination") ? "active" : ""
+            }`}
           >
             <Link to="/destination" className="uppercase tracking-2 text-white">
               <span className="mr-3 font-bold">01</span>Destination
             </Link>
           </li>
           <li
-            className={`underline-indicators ${location.pathname.startsWith("/crew") ? "active" : ""}`}
+            className={`underline-indicators ${
+              location.pathname.startsWith("/crew") ? "active" : ""
+            }`}
           >
             <Link to="/crew" className="uppercase tracking-2 text-white">
               <span className="mr-3 font-bold">02</span>Crew
             </Link>
           </li>
           <li
-            className={`underline-indicators ${location.pathname.startsWith("/technology") ? "active" : ""}`}
+            className={`underline-indicators ${
+              location.pathname.startsWith("/technology") ? "active" : ""
+            }`}
           >
-            <Link to="/technology" className="uppercase tracking-2 text-white">
+            <Link
+              to="/technology"
+              className="uppercase tracking-2 text-white"
+            >
               <span className="mr-3 font-bold">03</span>Technology
             </Link>
           </li>
