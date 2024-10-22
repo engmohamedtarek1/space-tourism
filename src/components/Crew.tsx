18b149bc9dc93cfe5b16b88f6eae3bffcab24540
engmohamedtarek1 <eng.mohamedtarek0@gmail.com>
2024-10-22 16:22:35 +0300

    Add types, Fix next button on focus

--- src/components/Crew.tsx
+++ src/components/Crew.tsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
-import { crew } from "../data";
+import React, { Dispatch, SetStateAction, useState } from "react";
+import { crew, CrewMember } from "../data";
 
 const Crew = () => {
-  const [currentCrewIndex, setCurrentCrewIndex] = useState(0);
-  const crewMember = crew[currentCrewIndex];
+  const [currentCrewIndex, setCurrentCrewIndex]: [
+    number,
+    Dispatch<SetStateAction<number>>,
+  ] = useState(0);
+  const crewMember: CrewMember = crew[currentCrewIndex];
 
   return (
     <div className="flex flex-col gap-12 p-6 text-center lg:ml-36 lg:text-left">
       {/* Title */}
-      <div className="tracking-[0.1rem] md:self-start md:pl-4 lg:pl-0 ">
-        <span className="pr-4 font-bold opacity-25">0{currentCrewIndex + 1}</span>MEET YOUR CREW
+      <div className="tracking-[0.1rem] md:self-start md:pl-4 lg:pl-0">
+        <span className="pr-4 font-bold opacity-25">
+          0{currentCrewIndex + 1}
+        </span>
+        MEET YOUR CREW
       </div>
 
       {/* Content */}
@@ -17,21 +23,25 @@ const Crew = () => {
         {/* Explanation */}
         <div className="flex flex-col items-center gap-6 lg:items-start">
           <div className="font-serif uppercase">
-            <p className="text-400 md:text-[1.5rem] lg:text-600 text-white/50">{crewMember.role}</p>
-            <p className="md:text-[2.5rem] text-[1.5rem] lg:text-700">{crewMember.name}</p>
+            <p className="text-400 text-white/50 md:text-[1.5rem] lg:text-600">
+              {crewMember.role}
+            </p>
+            <p className="text-[1.5rem] md:text-[2.5rem] lg:text-700">
+              {crewMember.name}
+            </p>
           </div>
-          <div className="md:w-[35rem] font-sans text-300 md:text-400 text-light">
+          <div className="font-sans text-300 text-light md:w-[35rem] md:text-400">
             <p>{crewMember.bio}</p>
           </div>
-          <div className="flex gap-4 lg:gap-10 mt-6 lg:mt-36">
+          <div className="mt-6 flex gap-4 lg:mt-36 lg:gap-10">
             {crew.map((_, index) => (
               <button
                 title="Next"
                 key={index}
                 onClick={() => setCurrentCrewIndex(index)}
-                className={`lg:h-4 lg:w-4 h-[0.625rem] w-[0.625rem] rounded-full ${
+                className={`h-[0.625rem] w-[0.625rem] rounded-full lg:h-4 lg:w-4 ${
                   currentCrewIndex === index ? "bg-white" : "bg-white/20"
-                } hover:bg-white/50`}
+                } hover:bg-white/50 focus:bg-white`}
               />
             ))}
           </div>
